3a3cc6f20051dd86f599767399370f5544768553
Isaiah Cruz <icruz@stratpoint.com>
2016-08-30 18:43:02 +0800

    Updated gulpfile.js Browserify, and used npm version of bootstrap and jquery Removed old bootstrap and jquery

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,49 @@
-var gulp = require('gulp'), 
-    concat = require('gulp-concat'), 
-    notify = require('gulp-notify');
+var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var gutil = require('gulp-util');
+var concat = require('gulp-concat');
+var notify = require('gulp-notify');
+var transform = require('vinyl-transform');
+var sass = require('gulp-sass');
 
 gulp.task('default', function() {
   // place code for your default task here
 });
 
 gulp.task('watch', function () {
-    gulp.watch('assets/css/*.css', ['css']);	
-    gulp.watch('assets/js/*.js', ['js']);  
-    gulp.start(['css', 'js']);
+    gulp.watch('assets/scss/app.scss', ['scss']);	
+    gulp.watch(['assets/js/*.js', 'assets/js/**/*'], ['javascript']);  
+    gulp.start(['scss', 'javascript']);
 });
 
-gulp.task('js', function() {
-	return gulp.src('assets/js/jquery-3.1.0.slim.min.js')
-        .pipe(gulp.src('assets/js/!(jquery-3.1.0.slim.min)*.js'))
-        .pipe(concat('app.js'))
-        .pipe(gulp.dest('public/build/'))
-        .pipe(notify('JS Compiled'));
-})
+gulp.task('javascript', function () {
+  // set up the browserify instance on a task basis
+  var b = browserify({
+    entries: 'assets/js/main.js',
+    debug: true
+  });
 
-gulp.task('css', function() {
-	return gulp.src('assets/css/*.css')
-    	.pipe(concat('app.css'))
+  return b.bundle()
+    .pipe(source('assets/js/*.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+        // Add transformation tasks to the pipeline here.
+        // .pipe(uglify())
+        .on('error', gutil.log)
+        .pipe(concat('app.js'))
+        .pipe(notify('Javascript Compiled'))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('public/build/'));
+});
+
+gulp.task('scss', function() {
+	return gulp.src('assets/scss/app.scss')
+        .pipe(sass())
+        .pipe(concat('app.scss'))
         .pipe(gulp.dest('public/build/'))
-        .pipe(notify('CSS Compiled'));
+        .pipe(notify('SCSS Compiled'));
 });
